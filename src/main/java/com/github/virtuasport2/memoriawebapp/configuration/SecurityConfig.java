8dcc673cdfe2b4c1042a97028788a6e17200409a
Massimiliano <virtuasport2@gmail.com>
2026-06-14 10:23:32 +0200

    Fix CORS + Security JWT

--- src/main/java/com/github/virtuasport2/memoriawebapp/configuration/SecurityConfig.java
+++ src/main/java/com/github/virtuasport2/memoriawebapp/configuration/SecurityConfig.java
@@ -1,8 +1,9 @@
 package com.github.virtuasport2.memoriawebapp.configuration;
-	
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,173 +22,165 @@ import com.github.virtuasport2.memoriawebapp.security.JwtAuthenticationFilter;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.http.HttpServletResponse;
 
-	
+import org.springframework.security.config.Customizer;
+
 //Indica a Spring che questa classe contiene delle configurazioni
 @Configuration
-//Abilita il supporto di Spring Security per l'applicazione web
+// Abilita il supporto di Spring Security per l'applicazione web
 @EnableWebSecurity
 public class SecurityConfig {
 
- // Inietta il filtro personalizzato che gestisce l'autenticazione basata su JWT
- @Autowired
- private JwtAuthenticationFilter jwtAuthenticationFilter;
+    // Inietta il filtro personalizzato che gestisce l'autenticazione basata su JWT
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
- // Definisce un bean che rappresenta la catena di filtri di sicurezza da applicare alle richieste HTTP
- @Bean
- public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-     http
-        
-         // Disabilita la protezione CSRF: non necessaria per API REST che utilizzano token (JWT)
-         .csrf(AbstractHttpConfigurer::disable)
-        
-         .logout(logout -> logout.disable()) // Disabilita il LogoutFilter di Spring
-         
-         // Configura le regole di autorizzazione per le richieste HTTP
-         .authorizeHttpRequests(auth -> auth
-        		 // Permette l'accesso anonimo agli endpoint di tipo ERROR e FORWARD
-                 .dispatcherTypeMatchers(DispatcherType.ERROR, DispatcherType.FORWARD).permitAll()   
-                 
-                 .requestMatchers("/api/auth/logout").permitAll()  // Consente l'accesso a /api/auth/logout senza autenticazione    
-                 
-             // Rende accessibile l'endpoint "/api/auth/login" a chiunque (necessario per il login)
+    // Definisce un bean che rappresenta la catena di filtri di sicurezza da
+    // applicare alle richieste HTTP
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
 
-             .requestMatchers("/api/tipi-documento",
-            		          "/api/auth/test-logout",
-            		          "/api/auth/test-logout1",
-            		          "/api/auth/logout",
-            		          "/api/auth/login",
-            		          "/api/utenti", 
-            		          "/api/auth/forgot-password").permitAll()
-             // solo utenti già autenticati possano accedere a /forgot-password
-             .requestMatchers("/forgot-password","/api/auth/logout").authenticated()
-             
+                // Disabilita la protezione CSRF: non necessaria per API REST che utilizzano
+                // token (JWT)
+                .csrf(AbstractHttpConfigurer::disable)
 
-             // Richiede che tutte le altre richieste siano eseguite da utenti autenticati
-             .anyRequest().authenticated()
+                .cors(Customizer.withDefaults())
 
-             
-         )
-         // Inserisce il filtro JWT personalizzato prima del filtro standard che gestisce l'autenticazione basata su username/password
-         // In questo modo, il filtro JWT ha la possibilità di processare il token e impostare l'autenticazione prima che il filtro standard intervenga
-         .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
-         
-         
-         
-//         .logout(logout -> logout
-//                 .logoutUrl("/api/auth/logout")
-//                 .logoutSuccessHandler((request, response, authentication) -> {
-//                     response.setStatus(HttpServletResponse.SC_OK);
-//                 })
-//                 .deleteCookies("JSESSIONID") // Se ci sono cookie da eliminare
-//                 .invalidateHttpSession(false) // Poiché stai usando JWT, non invalidare la sessione
-//              
-//             );
+                .logout(logout -> logout.disable()) // Disabilita il LogoutFilter di Spring
 
-     // Costruisce e restituisce la SecurityFilterChain configurata
-     return http.build();
- }
+                // Configura le regole di autorizzazione per le richieste HTTP
+                .authorizeHttpRequests(auth -> auth
+                        // Permette l'accesso anonimo agli endpoint di tipo ERROR e FORWARD
+                        .dispatcherTypeMatchers(DispatcherType.ERROR, DispatcherType.FORWARD).permitAll()
 
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
- 
+                        .requestMatchers(
+                                "/api/auth/login",
+                                "/api/auth/logout",
+                                "/api/utenti",
+                                "/api/tipi-documento",
+                                "/api/articoli",
+                                "/api/documento")
+                        .permitAll()
 
+                        // Richiede che tutte le altre richieste siano eseguite da utenti autenticati
+                        .anyRequest().authenticated()
 
+                )
+                // Inserisce il filtro JWT personalizzato prima del filtro standard che gestisce
+                // l'autenticazione basata su username/password
+                // In questo modo, il filtro JWT ha la possibilità di processare il token e
+                // impostare l'autenticazione prima che il filtro standard intervenga
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+        // .logout(logout -> logout
+        // .logoutUrl("/api/auth/logout")
+        // .logoutSuccessHandler((request, response, authentication) -> {
+        // response.setStatus(HttpServletResponse.SC_OK);
+        // })
+        // .deleteCookies("JSESSIONID") // Se ci sono cookie da eliminare
+        // .invalidateHttpSession(false) // Poiché stai usando JWT, non invalidare la
+        // sessione
+        //
+        // );
 
- // Definisce un bean di tipo PasswordEncoder che utilizza BCrypt
- @Bean
- public PasswordEncoder passwordEncoder() {
-     return new BCryptPasswordEncoder();
- }
+        // Costruisce e restituisce la SecurityFilterChain configurata
+        return http.build();
+    }
 
-	    
-	    
-//	    @Bean
-//	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//	        http.csrf(csrf -> csrf.disable())
-//	                .authorizeHttpRequests(requests -> requests
-//	                		.anyRequest().permitAll()  // Permette tutte le richieste senza autenticazione
-//	            //   .requestMatchers("/api/auth/**").permitAll() // Permetti login e registrazione senza token
-//	              //  		.requestMatchers("/api/utenti/**").permitAll()
-//	                		//	                .and()
-////	                .formLogin()
-////	                .loginPage("/login") // la pagina di login
-////	                .usernameParameter("username")
-////	                .passwordParameter("password")
-////	                .permitAll()
-//	                
-////	                 .anyRequest().authenticated())
-////	                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-////                     .addFilterBefore(jwtFilter, AnonymousAuthenticationFilter.class);
-//	                		 )
-//	                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//   
-//	        return http.build();
-//	    }
+    // Definisce un bean di tipo PasswordEncoder che utilizza BCrypt
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	
-	    
-	    @Bean
-	    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-	            throws Exception {
-	        return authenticationConfiguration.getAuthenticationManager();
-	    }
-	}
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+    // Exception {
+    // http.csrf(csrf -> csrf.disable())
+    // .authorizeHttpRequests(requests -> requests
+    // .anyRequest().permitAll() // Permette tutte le richieste senza autenticazione
+    // // .requestMatchers("/api/auth/**").permitAll() // Permetti login e
+    // registrazione senza token
+    // // .requestMatchers("/api/utenti/**").permitAll()
+    // // .and()
+    //// .formLogin()
+    //// .loginPage("/login") // la pagina di login
+    //// .usernameParameter("username")
+    //// .passwordParameter("password")
+    //// .permitAll()
+    //
+    //// .anyRequest().authenticated())
+    //// .sessionManagement(management ->
+    // management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    //// .addFilterBefore(jwtFilter, AnonymousAuthenticationFilter.class);
+    // )
+    // .sessionManagement(session ->
+    // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    //
+    // return http.build();
+    // }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
 
-//******************************************************************************************************
+// ******************************************************************************************************
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
 //
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeHttpRequests(auth -> auth
-//                .anyRequest().permitAll()  // Permette tutte le richieste
-//            )
-//            .csrf(csrf -> csrf.disable())  // Disabilita CSRF (non necessario per API REST)
-//            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+// @Bean
+// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+// Exception {
+// http
+// .authorizeHttpRequests(auth -> auth
+// .anyRequest().permitAll() // Permette tutte le richieste
+// )
+// .csrf(csrf -> csrf.disable()) // Disabilita CSRF (non necessario per API
+// REST)
+// .sessionManagement(session ->
+// session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //
-//        return http.build();
-//    }
-//}
+// return http.build();
+// }
+// }
 
-//******************************************************************************************************
+// ******************************************************************************************************
 
-//	 @Bean
-//	 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//       http
-//               .authorizeHttpRequests(authorize -> authorize
-//                               .requestMatchers("/api/documenti","/login", "/resources/**", "/css/**", "/js/**", "/images/**", "/public-api/**")
-//                               .permitAll() // Permetti accesso alle risorse statiche
-//                               
-//                               .requestMatchers("/api/documenti/**")
-//                               .authenticated()  // Richiede autenticazione per tutte le altre pagine
-//                               //.permitAll() // Nessuna autenticazione richiesta per /api/**
-//                               //.anyRequest() // Qualsiasi altra richiesta
-//                               //.permitAll() 
-//                              // .authenticated() // Autenticazione necessaria per il resto
-//                               
-//                               
-//               )
+// @Bean
+// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+// Exception {
+// http
+// .authorizeHttpRequests(authorize -> authorize
+// .requestMatchers("/api/documenti","/login", "/resources/**", "/css/**",
+// "/js/**", "/images/**", "/public-api/**")
+// .permitAll() // Permetti accesso alle risorse statiche
 //
-//              .httpBasic(basic -> basic.disable())  // Disabilita l'autenticazione di base
-//              .formLogin(login -> login.disable());  // Disabilita il login tramite form
-//         
-//               
-//               
+// .requestMatchers("/api/documenti/**")
+// .authenticated() // Richiede autenticazione per tutte le altre pagine
+// //.permitAll() // Nessuna autenticazione richiesta per /api/**
+// //.anyRequest() // Qualsiasi altra richiesta
+// //.permitAll()
+// // .authenticated() // Autenticazione necessaria per il resto
 //
-//       
-//       return http.build();
-//   }	
-//	
-	
+//
+// )
+//
+// .httpBasic(basic -> basic.disable()) // Disabilita l'autenticazione di base
+// .formLogin(login -> login.disable()); // Disabilita il login tramite form
+//
+//
+//
+//
+//
+// return http.build();
+// }
+//
 
-
-
-	
-
-//    
-   
-  
+//
